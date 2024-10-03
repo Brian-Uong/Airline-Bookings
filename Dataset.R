8259c0938daa0e9abc6379f06fa3c87b1e7ebc04
Brian Uong <uongb03@gmail.com>
2024-10-02 23:21:42 -0700

    Dataset

---
+++ Dataset.R
@@ -0,0 +1,8 @@
+## Dataset
+[Airlines Booking](https://www.kaggle.com/datasets/anandshaw2001/airlines-booking-csv)
+```{r, include =FALSE}
+# use path for customer_booking file 
+bookings <- read.csv("C:/Users/uongb/Documents/School/Junior/Spring/Stat 167/proj/FINAL PROJECT/customer_booking.csv")
+
+bookings
+```
